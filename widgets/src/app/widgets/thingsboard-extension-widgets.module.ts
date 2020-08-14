e8bb79bfd96caa710495a23f9fbb24146e14a671
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2020-08-14 18:59:34 +0300

    Update widget-extension example

--- widgets/src/app/widgets/thingsboard-extension-widgets.module.ts
+++ widgets/src/app/widgets/thingsboard-extension-widgets.module.ts
@@ -7,10 +7,14 @@ import { CommonModule } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 import addCustomWidgetLocale from './locale/custom-widget-locale.constant';
 import { ExampleModule } from './components/example/example.module';
+import { SharedModule } from '@shared/public-api';
+import { HomeComponentsModule } from '@home/components/public-api';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HomeComponentsModule,
+    SharedModule
   ],
   exports: [
     ExampleModule
