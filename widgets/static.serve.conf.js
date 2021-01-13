0f942177086d68037525ff794bbe2e849517a5f0
Dmitriymush <powder322@gmail.com>
2021-01-13 16:37:10 +0200

    Fixed: static_serve_config path

--- widgets/static.serve.conf.js
+++ widgets/static.serve.conf.js
@@ -2,10 +2,10 @@
  * Copyright © 2020 ThingsBoard
  */
 const STATIC_SERVE_CONFIG = {
-  '/static/thingsboard-extension-widgets.js': {
+  '/static/widgets/thingsboard-extension-widgets.js': {
     'target': 'dist/widget-extension/bundles/thingsboard-extension-widgets.umd.js'
   },
-  '/static/thingsboard-extension-widgets.umd.js.map': {
+  '/static/widgets/thingsboard-extension-widgets.umd.js.map': {
     'target': `dist/widget-extension/bundles/thingsboard-extension-widgets.umd.js.map`
   }
 }
